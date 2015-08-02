13bf66325472f28790aada856181fc5bfb50395d
Leon Wee <leonwee27@gmail.com>
2015-08-02 09:51:27 +0700

    Completed Inject Challenge

--- lib/inject_alternative.rb
+++ lib/inject_alternative.rb
@@ -1,10 +1,16 @@
-
 class Array
-  def inject_alternative(initial=nil)
-    answer = initial || first
-    self[0..-1].each do |x|
-      answer = yield(answer, x)
-    end
-    answer
+  def inject_alternative(initial = nil)
+    start = initial || first
+    start == initial ? self[0..-1].each {|x| start = yield(start, x)} : self[1..-1].each {|x| start = yield(start, x)}
+    start
+  end
+
+
+  def recursive_inject(initial=nil, &block)
+    start = initial || first
+    result = yield(start, first)
+    self.length == 1 ? result : self[1..-1].recursive_inject(result, &block)
   end
 end
+
+p [1,2,3,4].inject_alternative(1) { |x,y| x + y }
--- spec/inject_alternative_spec.rb
+++ spec/inject_alternative_spec.rb
@@ -5,11 +5,16 @@ describe Array do
     expect(subject).to respond_to(:inject_alternative).with(1).argument
   end
 
-  it "responds exactly like inject" do
+  it "responds exactly like inject for +" do
     array = [1,2,3,4]
     initial = 1
     inject = array.inject(initial) { |x, y| x + y}
     expect(array.inject_alternative(initial) { |x, y| x + y }).to eq inject
+  end
+
+  it "responds exactly like inject for + without block" do
+    inject = [5,6,7,8,9,10].inject { |sum, n| sum + n }
+    expect([5,6,7,8,9,10].inject_alternative { |sum, n| sum + n }). to eq inject
   end
 
 end
